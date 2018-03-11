d5df83f51de3a98ac7bc1525348c863871033e5b
a-pns <allonscotton@gmail>
2018-03-11 02:33:42 +0000

    Integrated Bing Search Into Entity Search API

--- EntitySearchApi/app/search.py
+++ EntitySearchApi/app/search.py
@@ -1,47 +1,59 @@
-# -*- coding: utf-8 -*-
-
 import http.client, urllib.parse
 import json
 
+
+subscriptionKey = '5a3cd3d67c67466ca30a2816510913e1'
+host = 'api.cognitive.microsoft.com'
+path = '/bing/v7.0/entities/'
+
+count = '10'
+offset = '0'
+safesearch = 'Moderate'
+
 def search_bing_api(entities):
-    subscriptionKey = '5a3cd3d67c67466ca30a2816510913e1'
-
-    host = 'api.cognitive.microsoft.com'
-    path = '/bing/v7.0/entities/'
-
-    mkt = 'en-GB'
-    count = '10'
-    offset = '0'
-    safesearch = 'Moderate'
-
-
-    # array = '{"entities":[{"name":"leptospiral infection","tag":"entity0","type":"MedicalCondition","textBeforeRefs":["sterilizing immunity for the animals from "],"textAfterRefs":["<\/body>"]},{"name":"inflammatory cells","tag":"entity1","type":"MedicalCondition","textBeforeRefs":["livers were infiltrated by various "],"textAfterRefs":[", indicating moderate to severe hepatitis. Focal"]},{"name":"immunizations","tag":"entity3","type":"MedicalTreatment","textBeforeRefs":["studies were tested for the ability of the ","et al., 2000 ). The hamster group that received "],"textAfterRefs":[" to generate an effective humoral response using"," containing recombinant chimeric protein,"]},{"name":"severe hepatitis","tag":"entity4","type":"MedicalCondition","textBeforeRefs":["inflammatory cells, indicating moderate to "],"textAfterRefs":[". Focal necrosis was also found in parenchymal"]},{"name":"Focal necrosis","tag":"entity5","type":"MedicalCondition","textBeforeRefs":["cells, indicating moderate to severe hepatitis. "],"textAfterRefs":[" was also found in parenchymal hepatocytes,"]},{"name":"e.g. multifocal pulmonary ecchymoses","tag":"entity6","type":"MedicalCondition","textBeforeRefs":["lesions on spirochete-targeted organs ("],"textAfterRefs":[", icteric liver and enlarged kidney). Lung"]},{"name":"immunization","tag":"entity10","type":"MedicalTreatment","textBeforeRefs":["analysis for LigB Ig-like domains utilized in ","with the negative control, LigB12. LigB10-B7-B7 ","kidney, and urinary bladder tissue from all ","all other hamsters from PBS, LigB7 and LigB10 "],"textAfterRefs":[" trials.(A) Far-UV circular dichroism (CD)"," led to an increase in post-booster secondary"," groups were examined by real-time quantitative"," groups presented severe clinical signs and"]},{"name":"immunization","tag":"entity10","type":"MedicalTreatment","textBeforeRefs":["analysis for LigB Ig-like domains utilized in ","with the negative control, LigB12. LigB10-B7-B7 ","kidney, and urinary bladder tissue from all ","all other hamsters from PBS, LigB7 and LigB10 "],"textAfterRefs":[" trials.(A) Far-UV circular dichroism (CD)"," led to an increase in post-booster secondary"," groups were examined by real-time quantitative"," groups presented severe clinical signs and"]},{"name":"Severe tubulointerstitial nephritis","tag":"entity11","type":"MedicalCondition","textBeforeRefs":["loss of normal tissue integrity ( Figure 7D ). "],"textAfterRefs":[" with locally extensive hemorrhage in uriniferous"]},{"name":"severe fibrosis","tag":"entity12","type":"MedicalCondition","textBeforeRefs":["by lymphoplasmacytic cells infiltration and "],"textAfterRefs":[". In contrast, all hamsters immunized with"]}]}'
-
-    #data = json.loads(array)
-
     entities = entities['entities']
-    print(entities)
     query = []
 
     for i in range(len(entities)):
-    	query.append(entities[i]['name'])
+        queryString = entities[i]['name']
+        json_results = send_bing_api_call(queryString, 'en-GB')
 
-    for i in range(len(query)):
-    	params = '?q=' + urllib.parse.quote (query[i]) + '&mkt=' + mkt + '&count=' + count + '&offset=' + offset + '&safesearch=' + safesearch
-    #urllib.parse.quote
+        if(json_results.get('entities',None)):
+            entities[i]['description'] = json_results['entities']['value'][0]['description']
+        else:
+            entities[i]['description'] = break_down_query_and_get_result(queryString)
 
-    	print("\n"+path+params+"\n")
+def send_bing_api_call(query, mkt):
 
-    	def get_suggestions ():
-    	    headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
-    	    conn = http.client.HTTPSConnection (host)
-    	    conn.request ("GET", path+params, None, headers)
-    	    response = conn.getresponse ()
-    	    return response.read ()
+    params = '?q=' + urllib.parse.quote(query) + '&mkt=' + mkt + '&count=' + count + '&offset=' + offset + '&safesearch=' + safesearch
+    headers = {'Ocp-Apim-Subscription-Key': subscriptionKey}
+    conn = http.client.HTTPSConnection (host)
+    conn.request ("GET", path+params, None, headers)
+    response = conn.getresponse()
 
-    	result = get_suggestions()
-    	json_results = json.loads(result)
-    	print(json_results)
+    print (path+params)
 
-    	if(json_results.get('entities',None)):
-    		entities[i]['description'] = json_results['entities']['value'][0]['description']
+    return json.loads(response.read())
+
+def break_down_query_and_get_result(query):
+    # remove and abreviations like e.g, etc
+    terms_array = query.split();
+    description = ""
+    is_first_description = True
+    for term in terms_array:
+        result = send_bing_api_call(term, 'en-GB')
+        if(result.get('entities',None)):
+            if is_first_description:
+                is_first_description = False
+            else:
+                description = description + "<br>"
+            description = description + term + " - " + result['entities']['value'][0]['description']
+        else:
+            result = send_bing_api_call(term, 'en-US')
+            if(result.get('entities', None)):
+                if is_first_description:
+                    is_first_description = False
+                else:
+                    description = description + "<br>"
+                description = description + term + " - " + result['entities']['value'][0]['description']
+    return description
