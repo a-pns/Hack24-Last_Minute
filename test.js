9683b92a0449fe64f19754b7d90e96313ca58f7b
Richard Hall <rhall93@gmail.com>
2018-03-11 03:55:50 +0000

    index html, css

---
+++ test.js
@@ -0,0 +1,20 @@
+var articleSectionIds = [];
+var changeArticleSections = {};
+for(var i=0; i < articleSections.length; i++) {
+  articleSectionIds[i] = articleSections[i].attr('id');
+}
+for(var i=0; i < articleSectionIds.length; i++) {
+  console.log(i, articleSectionIds[i]);
+}
+
+for(var i=0; i < entities.length; i++) {
+  while(indexOf(entities[i].name) > -1) {
+
+  }
+}
+
+if(document.innerHTML.indexOf(testEntities[0].name) > -1) {
+  index = document.innerHTML.indexOf(entities[0].name);
+  var thisEl = this;
+  thisEl.addClass('entity1');
+}
